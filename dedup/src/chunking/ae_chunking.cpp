3256db21fed6ed66b5de1a9ad314cb80e76a33ed
ababa <ababa@uwaterloo.ca>
2023-05-17 04:13:31 -0400

    remove commented out funtions

--- dedup/src/chunking/ae_chunking.cpp
+++ dedup/src/chunking/ae_chunking.cpp
@@ -59,51 +59,6 @@ uint64_t AE_Chunking::find_cutpoint(char* buff, uint64_t size) {
     return size;
 }
 
-// std::vector<File_Chunk> AE_Chunking::chunk_file(std::string file_path) {
-//     std::vector<File_Chunk> file_chunks;
-//     std::vector<char> buffer;
-//     std::ifstream file_ptr;
-//     file_ptr.open(file_path, std::ios::in);
-//     int64_t bytes_left = get_file_size(&file_ptr);
-//     // reserve 4 * max_size
-//     int64_t buffer_size = 65535;
-//     buffer.reserve(buffer_size << 2);
-//     int64_t chunk_size = (buffer_size << 1);
-//     // logical buffer end
-//     int64_t buffer_end = 0;
-
-//     while (true) {
-//         uint32_t bytes_to_read =
-//             std::min((int64_t)(buffer_size << 1), std::min(bytes_left, chunk_size));
-//         file_ptr.read(buffer.data() + buffer_end, bytes_to_read);
-//         if(file_ptr.gcount()==0){
-//             break;
-//         }
-//         buffer_end += bytes_to_read;
-//         chunk_size = find_cutpoint(buffer.data(), buffer_end);
-
-//         // create chunk
-//         File_Chunk new_chunk{chunk_size};
-//         memcpy(new_chunk.get_data(),buffer.data(), chunk_size);
-//         file_chunks.emplace_back(std::move(new_chunk));
-//         buffer_end -= chunk_size;
-//         memmove(&buffer[0], &buffer[chunk_size], buffer_end);
-//         bytes_left -= bytes_to_read;
-//     }
-//     // finalize 
-//     while(buffer_end > 0) {
-//         chunk_size = find_cutpoint(buffer.data(), buffer_end);
-//         buffer_end -= chunk_size;
-//         // create chunk
-//         File_Chunk new_chunk{chunk_size};
-//         memcpy(new_chunk.get_data(),buffer.data(), chunk_size);
-//         file_chunks.emplace_back(std::move(new_chunk));
-
-//         memmove(&buffer[0], &buffer[chunk_size], buffer_end);
-//     }
-
-//     return file_chunks;
-// }
 
 void AE_Chunking::chunk_stream(std::vector<File_Chunk>& result, std::istream& stream) {
     // uint64_t read_buff_end = 0;
